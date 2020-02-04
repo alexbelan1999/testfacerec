1a96a1e4010054bbf33a8b7ff2d5394654e5cc11
alexbelan1999 <alexbelan1999@gmail.com>
2020-02-04 21:44:20 +0300

    Test10

---
+++ test10.py
@@ -0,0 +1,91 @@
+import face_recognition
+import cv2
+
+input_movie = cv2.VideoCapture("Bill.mp4")
+length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+
+# Create an output movie file (make sure resolution/frame rate matches input video!)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_movie = cv2.VideoWriter('output.avi', fourcc, 29.97, (640, 360))
+
+bill_image = face_recognition.load_image_file("person/Bill_Gates01.jpg")
+elon_image = face_recognition.load_image_file("person/Elon_Musk01.jpg")
+steve_image = face_recognition.load_image_file("person/Steve_Jobs01.jpg")
+alex_image = face_recognition.load_image_file("person/Alex_Belan01.jpg")
+try:
+    bill_face_encoding = face_recognition.face_encodings(bill_image)[0]
+    elon_image_face_encoding = face_recognition.face_encodings(elon_image)[0]
+    steve_image_face_encoding = face_recognition.face_encodings(steve_image)[0]
+    alex_image_face_encoding = face_recognition.face_encodings(alex_image)[0]
+except IndexError:
+    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    quit()
+
+known_faces = [
+    bill_face_encoding,
+    elon_image_face_encoding,
+    steve_image_face_encoding,
+    alex_image_face_encoding
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+frame_number = 0
+
+while True:
+    # Grab a single frame of video
+    ret, frame = input_movie.read()
+    frame_number += 1
+
+    # Quit when the input video file ends
+    if not ret:
+        break
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+
+        # If you had more than 2 faces, you could make this logic a lot prettier
+        # but I kept it simple for the demo
+        name = None
+        if match[0]:
+            name = "Bill"
+        elif match[1]:
+            name = "Elon"
+        elif match[2]:
+            name = "Steve"
+        elif match[3]:
+            name = "Alex"
+
+        face_names.append(name)
+
+    # Label the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        if not name:
+            continue
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+    # Write the resulting image to the output video file
+    print("Writing frame {} / {}".format(frame_number, length))
+    output_movie.write(frame)
+
+# All done!
+input_movie.release()
+cv2.destroyAllWindows()
